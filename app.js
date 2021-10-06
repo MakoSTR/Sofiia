be015c3623cd49eb85ec7a1c7be7e779a2d2c27f
Sofiya Kravets <petrosyuk.sofiya@gmail.com>
2021-10-06 13:13:11 +0300

    base code

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-const fs = require('fs');
+// const fs = require('fs');
 const jsonData = require('./data.json');
 
-console.log(jsonData);
+const regularCustomer = jsonData['Regular customer'];
+const food = jsonData.Food;
+const baseingredients = jsonData['Base ingredients'];
+
+const fn = (name, order) => {
+        const allergies = regularCustomer[name];
+        const ingredients = food[order];
+        allergies.forEach(element => {
+            const foundAllergies = ingredients.find(item => {
+                if (item === element) {
+                    return true
+                }
+            });
+            if (foundAllergies) {
+                console.log(`${name} can’t order ${order}, allergic to: ${foundAllergies}`)
+            }
+            else console.log(`Hello ${name} - ${order}: success`)   
+           
+        });
+}
+
+fn('Julie Mirage', 'Fries');
+fn('Barbara Smith', 'Tuna Cake');
+fn('Bernard Unfortunate', 'Smashed Potatoes');
+fn('Julie Mirage', 'Fish In Water');
+fn('Elon Carousel', 'Fish In Water');
+
+
+
